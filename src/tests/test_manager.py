1be32734b68dca6e624b3b3d452f7b51182840a6
Jose Deodoro de Oliveira Filho <deodoro.filho@gmail.com>
2018-11-02 13:29:53 -0300

    Limpeza

--- src/tests/test_manager.py
+++
@@ -1,9 +0,0 @@
-from engine import manager
-
-def test_init():
-    m = manager.Manager()
-    assert m.players == {}
-    assert len([p for p in m.list_properties() if p != None]) == 28
-    for p in m.list_properties():
-        if p != None:
-            assert m.properties.who_owns(p._id)._id == m.properties.account._id
